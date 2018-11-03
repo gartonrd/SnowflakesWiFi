dc1d1137ac996d341752995a1c0b2bca2b54dbc5
Dean Garton <rdgarton@email.com>
2018-11-02 19:41:31 -0500

    add ability to change firmware Over The Air (OTA)

--- A0_ESP.ino
+++ A0_ESP.ino
@@ -5,13 +5,16 @@ Snowflakes WiFi
 ********************************************************/
 
 //version number
-  #define VERSION 0x0009
+  #define VERSION 0x000A
 
 //includes
   #include <Wire.h>
   #include <Ticker.h>
   #include <ESP8266WiFi.h>
   #include <ESP8266WebServer.h>
+  #include <ESP8266mDNS.h>
+  #include <WiFiUdp.h>
+  #include <ArduinoOTA.h>
 
 //defines
   #define START_OF_PATTERN_LENGTH 10;
@@ -97,6 +100,31 @@ void setup()
   
   StartWebServer(server);
 
+  ArduinoOTA.onStart([]()
+  {
+    Serial.println("OTA Start");
+  });
+  
+  ArduinoOTA.onEnd([]()
+  {
+    Serial.println("\nOTA End");
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  
+  ArduinoOTA.begin();
+  
   DisplayTestPattern(1000);
 
   //completely interrupt driven from timer
@@ -110,6 +138,7 @@ void loop()
 
   // Check for activity on HTTP server
   server.handleClient();
+  ArduinoOTA.handle();
   
   //if character typed
   if(Serial.available() > 0)
