134e7707dd012cd38c01849b1faee0dd38589412
Dean Garton <rdgarton@email.com>
2018-12-27 16:40:40 -0600

    reassign states in profile state machine

--- B3_SaveProfile.ino
+++ B3_SaveProfile.ino
@@ -17,55 +17,58 @@ void SaveProfile(uint8_t ProfileNumber8)
   uint16_t BlinkOnTime16;
   uint16_t BlinkOffTime16;
 
-  //initialize state and data
+  //initialize data
   StartIntensity8 = PatternRecord[2];
   StartIntensity16 = ScaleIntensity(StartIntensity8);
   StartIntensity[ProfileNumber8] = StartIntensity16;
+
+  EndIntensity8 = PatternRecord[3];
+  EndIntensity16 = ScaleIntensity(EndIntensity8);
+  EndIntensity[ProfileNumber8] = EndIntensity16;
+  
   RampTime16 = U8ToU16(PatternRecord[4], PatternRecord[5]);
-  if(RampTime16 == 0)
+  RampTime[ProfileNumber8] = RampTime16;
+  RampTime32 = RampTime16;
+  Denominator[ProfileNumber8] = (RampTime32) * (RampTime32);
+
+  BlinkDelayTime16 = U8ToU16(PatternRecord[6], PatternRecord[7]);
+  BlinkDelayTime[ProfileNumber8] = BlinkDelayTime16;  
+
+  BlinkOnTime16 = U8ToU16(PatternRecord[8], PatternRecord[9]);
+  BlinkOnTime[ProfileNumber8] = BlinkOnTime16;
+
+  BlinkOffTime16 = U8ToU16(PatternRecord[10], PatternRecord[11]);
+  BlinkOffTime[ProfileNumber8] = BlinkOffTime16;
+
+  //initialize state
+  State8 = 0x00;
+  if(BlinkOffTime16 != 0)
   {
-    State8 = 0x08;
-    BlinkOffTime16 = U8ToU16(PatternRecord[10], PatternRecord[11]);
-    BlinkOffTime[ProfileNumber8] = BlinkOffTime16;
-    if(BlinkOffTime16 == 0)
+    State8 |= 0x10;
+  }
+  if(BlinkOnTime16 != 0)
+  {
+    State8 |= 0x20;
+    if(BlinkDelayTime16 != 0)
     {
-      State8 |= 0x10;
-    }
-    BlinkOnTime16 = U8ToU16(PatternRecord[8], PatternRecord[9]);
-    BlinkOnTime[ProfileNumber8] = BlinkOnTime16;
-    if(BlinkOnTime16 == 0)
-    {
-      State8 |= 0x20;
-    }
-    else
-    {
-      BlinkDelayTime16 = U8ToU16(PatternRecord[6], PatternRecord[7]);
-      BlinkDelayTime[ProfileNumber8] = BlinkDelayTime16;
-      if(BlinkDelayTime16 != 0)
-      {
-        State8 |= 0x40;
-      }
+      State8 |= 0x80;
     }
   }
-  else
+
+  if(RampTime16 != 0)
   {
-    RampTime[ProfileNumber8] = RampTime16;
-    RampTime32 = RampTime16;
-    Denominator[ProfileNumber8] = (RampTime32) * (RampTime32);
-    EndIntensity8 = PatternRecord[3];
-    EndIntensity16 = ScaleIntensity(EndIntensity8);
-    EndIntensity[ProfileNumber8] = EndIntensity16;
     if(StartIntensity8 < EndIntensity8)
     {
-      State8 = 0x10;
+      State8 = 0x40;
       Numerator[ProfileNumber8] = EndIntensity16 - StartIntensity16;
     }
     else
     {
-      State8 = 0x20;
+      State8 = 0x50;
       Numerator[ProfileNumber8] = StartIntensity16 - EndIntensity16;
     }
   }
+  
   State[ProfileNumber8] = State8;
 }
 
